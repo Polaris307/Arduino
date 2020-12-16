1b107d431ae1b49888a9d237fc9d065d3d397dd0
Andrin Walla <andrin307@gmail.com>
2020-12-16 13:25:45 +0100

    Changed project type

--- dev.ino
+++ dev.ino
@@ -1,31 +1,85 @@
-#include <BH1750FVI.h>
-#include<HardwareSerial.h>
+int input_pin = 2;
+int led_pin = 3;
+boolean marantzIsOn;
+boolean marantzGotSwitchedOff = false;
 
-BH1750FVI LightSensor(BH1750FVI::k_DevModeContLowRes);
+byte leds = 0;
 
-
-void setup() 
-{
+void setup() {
   Serial.begin(115200);
-  LightSensor.begin();
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
+  pinMode(input_pin, INPUT);
+  pinMode(led_pin, OUTPUT);
+  Serial.println("initialized program");
+  digitalWrite(led_pin, LOW);
 }
 
-void loop()
-{
-  digitalWrite(13, HIGH);
-  delay(100);
-  uint16_t lux = LightSensor.GetLightIntensity();
-  digitalWrite(13, LOW);
-  Serial.print("Light: ");
-  Serial.println(lux);
-  if (lux<300)
-  {
-    digitalWrite(12, HIGH);
+void loop() {
+  if (ActiveMode() == true){
+    monitorMarantz(0);
+    if (marantzIsOn == true){
+    monitorMarantz(1);
+    }
+    if (marantzGotSwitchedOff == true){
+      switchOffTV();
+      marantzGotSwitchedOff = false;
+    }
+  }
+}
+
+boolean ActiveMode(){
+  return true;/*
+  if(digitalRead(input_pin) == HIGH){
+    statusLED(true);
+    return(true);
+  }
+  else if(digitalRead(switch_pin) == LOW){
+    statusLED(false);
+    return(false);
+  }
+}
+
+void statusLED(boolean led){
+  if(led == true){
+    digitalWrite(led_pin, HIGH);
   }
   else{
-    digitalWrite(12, LOW);
+    digitalWrite(led_pin, LOW);
+  }*/
+}
+
+void monitorMarantz(int marantzStatus){
+  delay(1000);
+  if (marantzStatus == 0){                     //Wenn du meinst, dass der Marantz aus ist, schau, ob der input pin doch HIGH ist. Wenn ja, setze den marantzison auf true
+      Serial.println("Checking Marantz status...");                   
+      if(digitalRead(input_pin) == HIGH){     //check input pin
+        Serial.println("Marantz is ON");
+        marantzIsOn = true;
+        return;
+      }
   }
-  delay(200);
+  if (marantzStatus == 1){                    //Wenn du weisst, dass der Marantz an ist, dann schau, ob der input pin auf low geht.
+    Serial.println("Entering ON Mode.");
+    while (marantzStatus == 1){
+      Serial.println("Checking if Marantz got switched off...");
+      if(digitalRead(input_pin) == LOW){      //check input pin
+        marantzIsOn = false;
+        marantzGotSwitchedOff = true;
+        marantzStatus = 0;
+        Serial.println("Marantz appears to be offline now.");
+      }
+      delay(1000);
+    }
+  }
+
+}
+
+void switchOffTV(){
+  Serial.println("Switching off TV...");
+  digitalWrite(led_pin, HIGH);
+  delay(100);
+  digitalWrite(led_pin, LOW);
+  delay(100);
+  digitalWrite(led_pin, HIGH);
+  delay(100);
+  digitalWrite(led_pin, LOW);
 }
