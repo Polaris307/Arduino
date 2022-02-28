ebc35910f24dabe2193d0279ee8cf4cd72150c24
Polaris307 <91419868+Polaris307@users.noreply.github.com>
2022-02-28 18:07:35 +0100

    Add files via upload
    Final version of source code.

--- dev.ino
+++ dev.ino
@@ -1,33 +1,30 @@
-#include<BH1750FVI.h>
+#include <IRremote.hpp>
 #include<HardwareSerial.h>
-
-BH1750FVI LightSensor(BH1750FVI::k_DevModeContLowRes);
+#define IR_SEND_PIN 5
 
 //ANPASSBARE VARIABLEN
 
 int ledThreshold = 100;                          //Wie hell muss die LED sein um als "an" zu gelten?
 int ledOffTime = 50;                             //Wie lange soll der Arduino bis zur nächsten Messung warten?
 int ledRepeats = 400;                            //Wie oft soll der Arduino Messen? ledRepeats*ledOffTime = So lange (in ms) muss LED aus sein, dass der Fernseher abgestellt wird.
-int powerSave = 10000;                           //Wie lange soll zwischen Messungen gewartet werden, wenn der AppleTV aus ist? 
+int powerSave = 1000;                           //Wie lange soll zwischen Messungen gewartet werden, wenn der AppleTV aus ist? 
 
 //GLOBALE VARIABLEN
 
-int switch_pin = 2;                              //Im Moment unbenutzt
-int led_pin = 3;                                 //Für fake-LED
+int switch_pin = 6;                              //MIT INPUT PIN TAUSCHEN FÜR FERTIGES PROGRAMM
+int led_pin = 3;                                 //Für System-LED
+int input_pin = 7;
 boolean AppleTvIsOn;
 boolean AppleTvGotSwitchedOff = false;           //AppleTvGotSwitchedOff ist die Schlüsselvariable. Ist sie true, wird auch der Fernseher ausgeschaltet.
 byte leds = 0;                                   //Nötig??? Wenn nicht, löschen.
 
 void setup() {
-  Serial.begin(115200);                     
-  LightSensor.begin();                      
-  pinMode(switch_pin, INPUT);                     //Im Moment unbenutzt.
+  Serial.begin(115200);                         
+  pinMode(switch_pin, INPUT);
   pinMode(led_pin, OUTPUT);
-  //EINSCHALTSEQUENZ
-  Serial.println("initialized program");
-  digitalWrite(led_pin, HIGH);
-  delay(500);
-  digitalWrite(led_pin, LOW);
+  pinMode(input_pin, INPUT);
+  Serial.println("started program");
+  IrSender.begin(IR_SEND_PIN, ENABLE_LED_FEEDBACK);
 }
 
 void loop() {
@@ -46,22 +43,19 @@ void loop() {
 void monitorAppleTv(int AppleTvStatus){         //Diese Funktion übernimmt die Schalterlogik und bestimmt nach 2 Kriterien ob der Apple TV im Moment aus oder an ist. (1:War er vorher an? && 2:Ist er jetzt an?)
   delay(1000);                                  //Eine 0 im AppleTvStatus heisst, der Arduino geht davon aus, dass der Apple TV aus ist. Eine 1, heisst das Gegenteil (Apple TV ist an.)
   if (AppleTvStatus == 0){                      //Wenn du meinst, dass der AppleTv aus ist, schau, ob der input pin doch HIGH ist. Wenn ja, setze den AppleTvIson auf true
-      Serial.println("Checking AppleTv status...");                   
-      if(statusLedOn() == true){                //check LED
-        Serial.println("AppleTv is ON");
+      feedback(1);                   
+      if(inputOn() == true){                //check LED
         AppleTvIsOn = true;
         return;
       }
   }
   if (AppleTvStatus == 1){                      //Wenn du weisst, dass der AppleTv an ist, dann schau, ob der input pin auf low geht.
-    Serial.println("Entering ON Mode.");
+    feedback(0);
     while (AppleTvStatus == 1){
-      Serial.println("Checking if AppleTv got switched off...");
-      if(statusLedOn() == false){               //check LED
+      if(inputOn() == false){               //check LED
         AppleTvIsOn = false;
         AppleTvGotSwitchedOff = true;
         AppleTvStatus = 0;
-        Serial.println("AppleTv appears to be offline now.");
         return;
       }
       delay(powerSave);                         //Wenn du weisst, dass der AppleTv an ist, miss weniger oft, um Energie zu sparen und den Sensor abkühlen zu lassen.
@@ -70,50 +64,64 @@ void monitorAppleTv(int AppleTvStatus){         //Diese Funktion übernimmt die 
 
 }
 
-boolean statusLedOn(){                          //Ist die LED am Apple TV an? (TRUE wenn ja, FALSE wenn nein.)
-  int repeats = 0;
-  int repeatStore = ledRepeats;                 //"Store"-Variablen speichern die Thresholdwerte, welche im Falle eines ausgeschalteten Apple TVs überschrieben werden.
-  int offStore = ledOffTime;                    //Die Thresholdwerte werden wieder zurückgesetzt, wenn der Messzyklus vorbei ist oder die LED an ist.
-  if(AppleTvIsOn==false){                       //Wenn du weisst, dass der AppleTv aus ist, miss weniger oft, um Energie zu sparen und den Sensor abkühlen zu lassen.
-      Serial.println("Apple TV is off, waiting with measurement.");               
-      ledRepeats=10;
-      ledOffTime=500;
-      delay(powerSave);
+boolean inputOn(){                          //Ist die LED am Apple TV an? (TRUE wenn ja, FALSE wenn nein.)
+if(digitalRead(input_pin)==HIGH){
+    return true;
   }
-  while(repeats<=ledRepeats){
-    uint16_t lux = LightSensor.GetLightIntensity();
-                              Serial.print("Measured Light Level: ");   //DEBUG!!! IN FINALEM CODE ENTFERNEN; MACHT ARDUINO WESENTLICH LANGSAMER
-                              Serial.println(lux);                      //DEBUG!!! IN FINALEM CODE ENTFERNEN; MACHT ARDUINO WESENTLICH LANGSAMER
-    if(lux>ledThreshold){                       //Wenn LED auch nur einen Moment an ist, returne true. (StatusLED "on")
-      repeats = 0;
-      Serial.println("Light level is high, Apple tv seems to be on");   
-      ledRepeats=repeatStore;
-      ledOffTime=offStore;
-      return true;
-    }
-    else if(repeats>=ledRepeats){               //Wenn LED über gesamten Messzeitraum aus ist, returne false. (StatusLED nicht "on")
-      repeats = 0;
-      Serial.println("Apple TV LED has been off for quite some time now. Apple TV seems to be off");
-      ledRepeats=repeatStore;
-      ledOffTime=offStore;
-      return false;
-      }
-    else{                                       //Wenn Messzeitraum noch nicht vorüber, miss noch einmal.
-      repeats++;
-      delay(ledOffTime);
-      }
+  else{
+    return false;    
   }
-
-
 }
 
 void switchOffTV(){                             //Übernimmt alles was mit dem Ausschalten des Fernsehers zusammenhängt.
-  Serial.println("Switching off TV...");
-  digitalWrite(led_pin, HIGH);
-  delay(100);
-  digitalWrite(led_pin, LOW);
-  delay(100);
-  digitalWrite(led_pin, HIGH);
-  delay(100);
-  digitalWrite(led_pin, LOW);
+   if(digitalRead(switch_pin) == 1){
+   feedback(2);
+   }
+   else{
+   IrSender.sendNEC(0x20DF10EF,32);
+   feedback(3);
+   //}
+}
+}
+void feedback(int type){
+  if(type == 0){
+    Serial.println("Entering ON Mode. | . -");
+    digitalWrite(led_pin, HIGH);
+    delay(100);
+    digitalWrite(led_pin, LOW);
+    delay(100);
+    digitalWrite(led_pin, HIGH);
+    delay(100);
+    digitalWrite(led_pin, LOW);
+    }
+  if(type == 1){
+    Serial.println("Checking AppleTv status... | . .");/*
+    digitalWrite(led_pin, HIGH);
+    delay(100);
+    digitalWrite(led_pin, LOW);
+    delay(100);
+    digitalWrite(led_pin, HIGH);
+    delay(100);
+    digitalWrite(led_pin, LOW);*/
+    }
+  if(type == 2){
+    Serial.println("Remote mode switched off by hardware switch. | - -");
+    digitalWrite(led_pin, HIGH);
+    delay(1000);
+    digitalWrite(led_pin, LOW);
+    delay(100);
+    digitalWrite(led_pin, HIGH);
+    delay(1000);
+    digitalWrite(led_pin, LOW);
+    }
+  if(type == 3){
+    Serial.println("Switching off TV... | - .");
+    digitalWrite(led_pin, HIGH);
+    delay(100);
+    digitalWrite(led_pin, LOW);
+    delay(100);
+    digitalWrite(led_pin, HIGH);
+    delay(1000);
+    digitalWrite(led_pin, LOW);
+    }
 }
